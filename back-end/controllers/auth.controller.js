a99697211a252675b5d859bcd3d3e36dd7ebd53b
ratan.aniket09@gmail.com <ratan.aniket09@gmail.com>
2024-06-03 21:10:17 +0530

    built CURD operation

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -1,17 +1,108 @@
 /** @format */
 
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "you hit the signup endpoint",
-  });
+  try {
+    const { fullName, username, email, password } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format." });
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Username is already taken." });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already taken." });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 6 characters long." });
+    }
+
+    // Hashing the password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImage: newUser.profileImage,
+        coverImage: newUser.coverImage,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data." });
+    }
+  } catch (error) {
+    console.log("Error in signup:", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
 };
+
 export const login = async (req, res) => {
-  res.json({
-    data: "you hit the login endpoint",
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    const isPasswordValidate = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user.username || !isPasswordValidate) {
+      return res
+        .status(400)
+        .json({ message: "Invalidate Username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImage: user.profileImage,
+      coverImage: user.coverImage,
+    });
+  } catch (error) {
+    console.log("Error in login:", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
 };
 export const logout = async (req, res) => {
-  res.json({
-    data: "you hit the logout endpoint",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out Successfully." });
+  } catch (error) {
+    console.log("Error in logout controller:", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
 };
