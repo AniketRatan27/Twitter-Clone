0e5142dba5174b6eeb01bc88ca48f8909ae4c35b
ratan.aniket09@gmail.com <ratan.aniket09@gmail.com>
2024-06-06 01:22:17 +0530

    built the posts routes and added controllers

---
+++ back-end/routes/post.routes.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  commentOnPost,
+  deletePost,
+  getAllPost,
+  getFollowingPosts,
+  getLikedPost,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.contoller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPost);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPost);
+
+router.post("/create", protectRoute);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
