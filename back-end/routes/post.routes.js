7439b5abeac4a4dda56349199a20686d17a0aad1
ratan.aniket09@gmail.com <ratan.aniket09@gmail.com>
2024-06-15 20:32:21 +0530

    created the delete post feature

--- back-end/routes/post.routes.js
+++ back-end/routes/post.routes.js
@@ -4,6 +4,7 @@ import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import {
   commentOnPost,
+  createPost,
   deletePost,
   getAllPost,
   getFollowingPosts,
@@ -19,7 +20,7 @@ router.get("/user/:username", protectRoute, getUserPosts);
 router.get("/following", protectRoute, getFollowingPosts);
 router.get("/likes/:id", protectRoute, getLikedPost);
 
-router.post("/create", protectRoute);
+router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
