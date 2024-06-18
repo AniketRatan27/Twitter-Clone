b5183f24233503b62fe9a3db88f7250818a7cf2d
ratan.aniket09@gmail.com <ratan.aniket09@gmail.com>
2024-06-19 00:42:29 +0530

    Implemented the notifications feature

--- back-end/controllers/notification.controller.js
+++ back-end/controllers/notification.controller.js
@@ -1,9 +1,8 @@
 /** @format */
 
-import { json } from "express";
 import Notification from "../models/notification.model.js";
 
-export const getNotifications = async (res, req) => {
+export const getNotifications = async (req, res) => {
   try {
     const userId = req.user._id;
 
@@ -17,7 +16,7 @@ export const getNotifications = async (res, req) => {
     res.status(200).json(notification);
   } catch (error) {
     console.log("Error in getNotifications: ", error.message);
-    res.status(500).json({ error: "Internal Server error." });
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
--- back-end/server.js
+++ back-end/server.js
@@ -5,12 +5,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongo from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -22,13 +29,14 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
